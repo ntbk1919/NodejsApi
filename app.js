cbcc774c992de21a9d24e74e3879880a5c84e701
ntbk1919 <noeltbejoy@gmail.com>
2020-08-28 10:25:46 +0530

    all http apis created

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express=require('express');
 const mongoose=require('mongoose');
 require('dotenv/config')
 const app=express();
+const cors=require('cors')
 //IMPORT ROUTES
 const postsRoute =require('./routes/posts');
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json());
 app.use('/posts',postsRoute);
 
 
@@ -16,6 +20,7 @@ app.get('/',(req,res)=>{
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION,
 {useNewUrlParser:true},
+
 ()=>
 console.log('Connected to db')
 );
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,72 @@
 const express=require('express');
+const Post=require('../models/Post')
+
 const router=express.Router();
-router.get('/',(req,res)=>{
-    res.send('We are on Posts')
+//GETS BACK ALL POSTS
+router.get('/',async(req,res)=>{
+    try{
+        const posts=await Post.find();
+        res.json(posts)
+    }
+    catch(err){
+        res.json({message:err})
+
+    }
+    
 });
-router.get('/specific',(req,res)=>{
-    res.send('We are on Specific  Post')
+router.post('/',async(req,res)=>{
+    const post=new Post({
+        title:req.body.title,
+        description:req.body.description
+    });
+    try{
+   const savedPost=await post.save()
+   res.json(savedPost) 
+    }
+    catch(err){
+        res.json({message:err})
+    }
+   
+    
+})
+//SPECIFIC POST
+router.get('/:postId',async(req,res)=>{
+    try{
+   const post= await Post.findById(req.params.postId);
+   res.json(post);
+    }
+    catch(err){
+        res.json({message:err})
+
+    }
+
+
+})
+//DELETE POST
+router.delete('/:postId',async(req,res)=>{
+    try{
+   const removedPost= await Post.remove({_id:req.params.postId});
+   res.json(removedPost);
+    }
+    catch(err){
+        res.json({message:err})
+
+    }
+
+
 });
+//UPDATE A POST
+router.patch('/:postId',async(req,res)=>{
+    try{
+   const updatedPost= await Post.updateOne({_id:req.params.postId},
+    {$set:{title:req.body.title}}
+    );
+   res.json(updatedPost);
+    }
+    catch(err){
+        res.json({message:err})
+
+    }
+}
+);
 module.exports=router;
